5f8ed6c370378975f500fc5be94d17fdd40762af
Chamber6821 <vania.osinin@yandex.ru>
2022-01-17 22:21:19 +0300

    test: change positions for match empty function

--- src/lexer/Lexer.spec.ts
+++ src/lexer/Lexer.spec.ts
@@ -23,9 +23,9 @@ test("Match empty function", () => {
         new Token(TokenType.Function, "func", { line: 1, column: 1 }),
         new Token(TokenType.Space, " ", { line: 1, column: 5 }),
         new Token(TokenType.Name, "myFunc", { line: 1, column: 6 }),
-        new Token(TokenType.Space, " ", { line: 1, column: 11 }),
-        new Token(TokenType.LBrace, "{", { line: 1, column: 12 }),
-        new Token(TokenType.RBrace, "}", { line: 1, column: 13 })
+        new Token(TokenType.Space, " ", { line: 1, column: 12 }),
+        new Token(TokenType.LBrace, "{", { line: 1, column: 13 }),
+        new Token(TokenType.RBrace, "}", { line: 1, column: 14 })
     ]
 
     expect(new Lexer(emptyProcedure).getAllTokens()).toEqual(expectedTokens)
