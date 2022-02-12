8224d4d4651f62a799b3185c569f71abd2195d10
Chamber6821 <vania.osinin@yandex.ru>
2022-02-12 22:23:57 +0300

    reafctor: remove class Collector

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -1,18 +1,17 @@
 import Scope        from "parser/nodes/Scope"
 import ParsingError from "parser/ParsingError"
-import Collector     from "utils/Collector"
 import TokenProvider from "parser/TokenProvider"
 
 
 export default class Parser {
-    private readonly errorCollector = new Collector<ParsingError>()
+    private collectedErrors: ParsingError[] = []
 
     constructor(
         private readonly provider: TokenProvider,
     ) {}
 
     get errors(): readonly ParsingError[] {
-        return this.errorCollector.collected
+        return this.collectedErrors
     }
 
     buildIncompleteTree(): Scope {
